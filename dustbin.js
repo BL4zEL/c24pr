1ea772fcc8014d4c3c8bf05f1419e779925bb3df
BL4zEL <70089424+BL4zEL@users.noreply.github.com>
2020-10-26 19:00:53 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,20 @@
+class Base{
+  constructor(x, y, width, height) {
+    var options = {
+        
+        'friction':0.3,
+        'isStatic':true
+      }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    rectMode(CENTER);
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+};
+
